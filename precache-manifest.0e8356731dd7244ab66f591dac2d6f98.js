1dcea29fc362ee556b691c5eef624baa7f69b8c1
HossamMuhammedOmar <hossamomar9876@gmail.com>
2020-09-07 16:07:03 +0200

    Updates

---
+++ precache-manifest.0e8356731dd7244ab66f591dac2d6f98.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2df6a759a1452ea0504313993233471b",
+    "url": "/color-box/index.html"
+  },
+  {
+    "revision": "83e863d32108ca071780",
+    "url": "/color-box/static/css/main.19c57873.chunk.css"
+  },
+  {
+    "revision": "079a43761c6c4dd5c066",
+    "url": "/color-box/static/js/2.cb9f7c19.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/color-box/static/js/2.cb9f7c19.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "83e863d32108ca071780",
+    "url": "/color-box/static/js/main.5bf82679.chunk.js"
+  },
+  {
+    "revision": "6d47664ffaa6e8b783fa",
+    "url": "/color-box/static/js/runtime-main.57e5539a.js"
+  }
+]);
